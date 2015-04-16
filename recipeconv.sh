269e196875afef597b93d48e6486e859f7c5ddb3
Robert Carlsen <rwcarlsen@gmail.com>
2015-04-16 16:10:34 -0500

    clean up

--- recipeconv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-name=$1
-file=$2
-echo '<recipe>'
-echo "    <name>$name</name>"
-echo "    <basis>mass</basis>"
-awk '$2 > 0 {printf "    <nuclide> <id>%s</id> <comp>%f</comp> </nuclide>\n",$1,$2}' $file
-echo '</recipe>'
