d476ae8c4d93438782e04481774d5f050543c826
Robert Carlsen <rwcarlsen@gmail.com>
2015-05-06 11:22:39 -0500

    fix eg23 scenario retirements - they were shifted 10 years

--- retiresched.sh
+++ retiresched.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "<prototypes>"
-tail -n+2 $1 | awk '$2 > 0 {print "<val>slow_reactor</val>"}'
+tail -n+2 $1 | awk '$2 > 0 {print "    <val>slow_reactor</val>"}'
 echo "</prototypes>"
 
 echo "<n_build>"
-tail -n+2 $1 | awk '$2 > 0 {printf "<val>%d</val>\n",$2}'
+tail -n+2 $1 | awk '$2 > 0 {printf "    <val>%d</val>\n",$2}'
 echo "</n_build>"
 
 # build initial reactors on time step 1
 echo "<build_times>"
-tail -n+2 $1 | awk '$2 > 0 {printf "<val>%d</val>\n",1}'
+tail -n+2 $1 | awk '$2 > 0 {printf "    <val>%d</val>\n",1}'
 echo "</build_times>"
 
 echo "<lifetimes>"
-tail -n+2 $1 | awk '$2 > 0 {printf "<val>%d</val>\n",($1-2005)*12}'
+tail -n+2 $1 | awk '$2 > 0 {printf "    <val>%d</val>\n",($1-2015)*12}'
 echo "</lifetimes>"
